aa1c91e106fb35b5aa99da5db4e6f3a5c2f1e539
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-09 23:47:45 +0530

    chore: establish ESLint code quality and formatting rules

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+/* * Author: Aditya Pandey 
+ * GitHub: https://github.com/iamAditya-Pandey 
+ * Description: ESLint configuration for maintaining code quality and formatting. 
+ */
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+])
