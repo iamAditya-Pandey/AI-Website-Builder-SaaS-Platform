fb96c4ddd1e8c117763dd544234e365261f25fb3
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-09 23:47:43 +0530

    feat(preview): create full-screen interactive deployment viewer

---
+++ src/pages/Preview.tsx
@@ -0,0 +1,60 @@
+/* * Author: Aditya Pandey 
+ * GitHub: https://github.com/iamAditya-Pandey 
+ * Description: Full-screen interactive preview environment for deployed components. 
+ */
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Loader2Icon } from "lucide-react";
+import ProjectPreview from "../components/ProjectPreview";
+import type { Project, Version } from "../types";
+import api from "@/configs/axios";
+import { toast } from "sonner";
+import { authClient } from "@/lib/auth-client";
+
+
+const Preview = () => {
+
+  const {data: session, isPending} = authClient.useSession()
+  const { projectId, versionId } = useParams()
+  const [code, setCode] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const fetchCode = async () => {
+   try {
+    const { data } = await api.get(`/api/project/preview/${projectId}`)
+    setCode(data.project.current_code)
+    if(versionId){
+      data.project.versions.forEach((version: Version)=>{
+        if(version.id === versionId){
+          setCode(version.code)
+        }
+      })
+    }
+    setLoading(false)
+   } catch (error: any) {
+    toast.error(error?.response?.data?.message || error.message);
+    console.log(error);
+   }
+  }
+
+  useEffect(()=>{
+    if(!isPending && session?.user){
+      fetchCode()
+    }
+  },[session?.user])
+
+  if(loading){
+    return (
+      <div className='flex items-center justify-center h-screen font-sans'>
+        <Loader2Icon className='size-8 animate-spin text-orange-400' />
+      </div>
+    )
+  }
+  return (
+    <div className="h-screen font-sans">
+      {code && <ProjectPreview project={{current_code: code} as Project} isGenerating={false} showEditorPanel={false}/>}
+    </div>
+  )
+}
+
+export default Preview
