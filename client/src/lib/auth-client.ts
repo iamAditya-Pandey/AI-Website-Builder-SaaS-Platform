44b46b26ff9ff14985123b8459a47ea635a9135d
iamAditya-Pandey <pandeyaditya19012006@gmail.com>
2026-03-11 03:51:51 +0530

    Refactor auth client setup by removing comments

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,12 +1,5 @@
-/* * Author: Aditya Pandey 
- * GitHub: https://github.com/iamAditya-Pandey 
- * Description: Authentication client setup for managing user sessions. 
- */
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react" 
 
 export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_BASEURL,
-    fetchOptions: {credentials: 'include'},
+    baseURL: import.meta.env.VITE_BASEURL, 
 })
-
-export const { signIn, signUp, useSession } = authClient;
